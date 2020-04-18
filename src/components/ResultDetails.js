a92f386a337f0382ff289aad4af0ade5b09f392d
Luis Diniz <dti425@localiza.com>
2020-04-18 16:29:18 -0300

    Definição de roteamento e criação de componentes básicos concluída

---
+++ src/components/ResultDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResultDetails  = (props) => {
+    return(
+        <div>
+            List of repositories and user info
+        </div>
+    );
+}
+
+export default ResultDetails;
---
+++ src/components/UserNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserNotFound  = (props) => {
+    return(
+        <div>
+            Username not found
+        </div>
+    );
+}
+
+export default UserNotFound;
