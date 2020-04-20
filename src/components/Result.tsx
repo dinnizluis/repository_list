7153261245f824b8f35fa8e3e722618f18ed556c
Luis Diniz <dti425@localiza.com>
2020-04-20 12:49:04 -0300

    Loader incluído e fonte do text field alterada para o protótipo

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -12,7 +12,6 @@ import Loader from 'react-loader-spinner';
 const useStyles = makeStyles({
     container: {
         flexDirection: 'row',
-        // marginLeft: '35px',
         width: '100%',
     },
     bodyContainer: {
@@ -31,7 +30,6 @@ const useStyles = makeStyles({
         letterSpacing: 'normal',
         color: '#000000',
         alignSelf: 'flex-start',
-        // marginLeft: '25px',
     },
     githubSearchTextStyle: {
         flex: 6,
@@ -65,7 +63,6 @@ const useStyles = makeStyles({
         height: '50px',
         backgroundColor: 'white',
         margin: 0,
-        // paddingTop: '35px',
     },
     searchButton: { 
         width: '100px', 
@@ -90,7 +87,6 @@ const Result  = (props) => {
     const classes = useStyles();
 
     const getData = async () => {
-        console.log('getting data : ', username);
         try  {
             let info = await getUserInfo(username);
             let repos = await getUserRepos(username);
@@ -99,13 +95,11 @@ const Result  = (props) => {
             setNotFound(false);
             setLoaded(true);
             const fullPath = ROUTES.RESULT + username;
-            console.log('fullpath : ', fullPath);
             props.history.push(fullPath);
         } catch {
             setNotFound(true);
             setLoaded(true);
             const fullPath = ROUTES.RESULT + username + ROUTES.NOT_FOUND;
-            console.log('fullpath : ', fullPath);
             props.history.push(fullPath);
         }
     }
@@ -120,13 +114,11 @@ const Result  = (props) => {
 
     useEffect(() => {
         if(username.length > 0){
-            console.log('username changed : ', username);
             getData();
         }
     }, [username]);
 
     useEffect(() => {
-        console.log('Ao construir o componente');
         let user: string = props.history.location.pathname.toString();
         user = user.substring(17);
         setUsername(user);
@@ -143,6 +135,9 @@ const Result  = (props) => {
                         className={classes.inputSearch}
                         value={username}
                         onChange={handleInputChange()}
+                        inputProps={{
+                            style: {fontSize: '18px', fontFamily:  'Raleway', color: '#5c5c5c', fontWeight: 300,},
+                        }}
                     >
                     </TextField>
                     <Button 
