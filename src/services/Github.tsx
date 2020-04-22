de51ea6f0ad98b7c7bb5d5aee8f0e1a2c781a169
Luis Diniz <dti425@localiza.com>
2020-04-22 09:00:00 -0300

    Adicionando ordenação descrecente à lista de repositórios

--- src/services/Github.tsx
+++ src/services/Github.tsx
@@ -1,6 +1,25 @@
 const axios = require('axios').default;
 const baseUrl = `https://api.github.com/users/`;
 
+function dynamicsort(property,order) {
+    var sort_order = 1;
+    if(order === "desc"){
+        sort_order = -1;
+    }
+    return function (a, b){
+        // a should come before b in the sorted order
+        if(a[property] < b[property]){
+                return -1 * sort_order;
+        // a should come after b in the sorted order
+        }else if(a[property] > b[property]){
+                return 1 * sort_order;
+        // a and b are the same
+        }else{
+                return 0 * sort_order;
+        }
+    }
+}
+
 function getStatusCode(err) {
     let code = 'status code ';
     let statusTxt = err.search(code);
@@ -41,8 +60,23 @@ async function getUserRepos(username: string) {
     catch (res) {
         retorno.value = (JSON.stringify(res));
         retorno.status = getStatusCode(retorno.value);
-        return retorno;
+        throw retorno;
     }
 }
 
-export {getUserInfo, getUserRepos};
+async function getUserReposSortedByStars(username: string) {
+    try {
+        let ret = await getUserRepos(username);
+        let sorted = JSON.parse(ret.value);
+        sorted = sorted.sort((dynamicsort("stargazers_count", "desc")));
+        ret.value = JSON.stringify(sorted);
+        console.log('sorted = ', sorted);
+        return ret;
+    }
+    catch (res){
+        throw res;
+    }
+
+}
+
+export {getUserInfo, getUserRepos, getUserReposSortedByStars};
